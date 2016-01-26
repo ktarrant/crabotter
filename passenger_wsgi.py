46a8f895636dcdf0af901975a08e94318a7d5680
kmtarrant13@gmail.com <kmtarrant13@gmail.com>
2016-01-26 01:06:52 -0500

    Huge changes, but Django demo is working now

---
+++ passenger_wsgi.py
@@ -0,0 +1,18 @@
+import sys, os
+INTERP = "/home/ktarrant/crabotter.com/env/bin/python"
+#INTERP is present twice so that the new python interpreter knows the actual executable path
+if sys.executable != INTERP:
+	print(sys.argv)
+	os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/crabdemo')  #You must add your project here
+
+sys.path.insert(0,cwd+'/env/bin')
+sys.path.insert(0,cwd+'/env/lib/python2.7/site-packages/django')
+sys.path.insert(0,cwd+'/env/lib/python2.7/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "crabdemo.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
