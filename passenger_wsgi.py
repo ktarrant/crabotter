7f7bbef5fa5fa059e7a4a100fb44c51eef184a1a
kmtarrant13@gmail.com <kmtarrant13@gmail.com>
2016-01-30 17:05:44 -0500

    Refactor settings system

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,12 +7,12 @@ if sys.executable != INTERP:
 
 cwd = os.getcwd()
 sys.path.append(cwd)
-sys.path.append(cwd + '/crabdemo')  #You must add your project here
+sys.path.append(cwd + '/health')  #You must add your project here
 
 sys.path.insert(0,cwd+'/env/bin')
 sys.path.insert(0,cwd+'/env/lib/python2.7/site-packages/django')
 sys.path.insert(0,cwd+'/env/lib/python2.7/site-packages')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "crabdemo.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "health.settings.prod"
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
